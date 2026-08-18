7f3f3ece1ee5586a82856bcc072ce90dbbb1ba07
Quilasd <forestloq@gmail.com>
2026-08-18 19:03:02 +0300

    Add continent selection to registration

--- BOT/app/bot/states/registration.py
+++ BOT/app/bot/states/registration.py
@@ -3,6 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class RegistrationStates(StatesGroup):
     choosing_world = State()
+    choosing_continent = State()
     choosing_name = State()
     choosing_gender = State()
     choosing_race = State()
