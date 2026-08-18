667f5c22eab9554c58aa8c29c8c832c60033a2af
Quilasd <forestloq@gmail.com>
2026-08-18 18:48:50 +0300

    Export database models

---
+++ BOT/app/database/models/__init__.py
@@ -0,0 +1,5 @@
+from app.database.models.user import User
+from app.database.models.world import World
+from app.database.models.world_member import WorldMember
+
+__all__ = ["User", "World", "WorldMember"]
