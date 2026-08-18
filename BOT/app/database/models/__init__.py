141d09bc884731e1c4cdc8006791fd6428148276
Quilasd <forestloq@gmail.com>
2026-08-18 18:57:13 +0300

    Register character model

--- BOT/app/database/models/__init__.py
+++ BOT/app/database/models/__init__.py
@@ -1,5 +1,6 @@
 from app.database.models.user import User
 from app.database.models.world import World
 from app.database.models.world_member import WorldMember
+from app.database.models.character import Character
 
-__all__ = ["User", "World", "WorldMember"]
+__all__ = ["User", "World", "WorldMember", "Character"]
