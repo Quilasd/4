52b9f0550f6da591743339b9d78457818b4168fc
Quilasd <forestloq@gmail.com>
2026-08-18 19:11:06 +0300

    Register game persistence models

--- BOT/app/database/models/__init__.py
+++ BOT/app/database/models/__init__.py
@@ -2,5 +2,15 @@ from app.database.models.user import User
 from app.database.models.world import World
 from app.database.models.world_member import WorldMember
 from app.database.models.character import Character
+from app.database.models.character_stat import CharacterStat
+from app.database.models.mastery import CharacterMastery
+from app.database.models.item import Item
+from app.database.models.inventory import InventoryItem
+from app.database.models.equipment import CharacterEquipment
+from app.database.models.state import CharacterResource
 
-__all__ = ["User", "World", "WorldMember", "Character"]
+__all__ = [
+    "User", "World", "WorldMember", "Character", "CharacterStat",
+    "CharacterMastery", "Item", "InventoryItem", "CharacterEquipment",
+    "CharacterResource",
+]
